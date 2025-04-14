d8306a9fc306ec76f76437978d9272446b8b5d14
Dmitrii Korotovskii <dmitry@korotovsky.io>
2025-04-14 14:19:29 +0200

    apply limit

--- internal/handler/conversations.go
+++ internal/handler/conversations.go
@@ -7,7 +7,15 @@ import (
 	"github.com/korotovsky/slack-mcp-server/internal/provider"
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/slack-go/slack"
+	"strconv"
 )
+
+type Message struct {
+	User    string `json:"user"`
+	Text    string `json:"text"`
+	Channel string `json:"channel"`
+	Time    string `json:"time"`
+}
 
 type ConversationsHandler struct {
 	apiProvider *provider.ApiProvider
@@ -25,21 +33,49 @@ func (ch *ConversationsHandler) ConversationsHistoryHandler(ctx context.Context,
 		return nil, errors.New("channel must be a string")
 	}
 
+	// tmp test
+	channel = "C01B6N9FZTN"
+
+	limit, ok := request.Params.Arguments["limit"].(string)
+	if !ok {
+		return nil, errors.New("channel must be a string")
+	}
+
 	api, err := ch.apiProvider.Provide()
 	if err != nil {
 		return nil, err
 	}
 
+	var (
+		messagesLimit int
+	)
+	if limit != "" {
+		messagesLimit, err = strconv.Atoi(limit)
+		if err != nil {
+			return nil, errors.New("can not parse limit")
+		}
+	}
+
 	params := slack.GetConversationHistoryParameters{
 		ChannelID: channel,
-		Limit:     3,
+		Limit:     messagesLimit,
 	}
 	messages, err := api.GetConversationHistoryContext(ctx, &params)
 	if err != nil {
 		return nil, err
 	}
 
-	data, err := json.MarshalIndent(messages.Messages, "", "    ")
+	var messageList []Message
+	for _, message := range messages.Messages {
+		messageList = append(messageList, Message{
+			User:    message.User,
+			Text:    message.Text,
+			Channel: channel,
+			Time:    message.Timestamp,
+		})
+	}
+
+	data, err := json.MarshalIndent(messageList, "", "    ")
 	if err != nil {
 		return nil, err
 	}
