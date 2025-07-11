2f574d68cd4aa361d2ee4fa701b0b620abab0661
Dmitrii Korotovskii <dmitry@korotovsky.io>
2025-07-11 21:59:23 +0200

    Fixes

--- npm/slack-mcp-server/bin/index.js
+++ npm/slack-mcp-server/bin/index.js
@@ -13,11 +13,27 @@ const BINARY_MAP = {
 
 // Resolving will fail if the optionalDependency was not installed or the platform/arch is not supported
 const resolveBinaryPath = () => {
+    // If DXT installation then we fix empty variables, it's DXT bug.
+    if (process.env.SLACK_MCP_DXT) {
+        if (process.env.SLACK_MCP_XOXC_TOKEN === '${user_config.xoxc_token}') {
+            process.env.SLACK_MCP_XOXC_TOKEN = '';
+        }
+        if (process.env.SLACK_MCP_XOXD_TOKEN === '${user_config.xoxd_token}') {
+            process.env.SLACK_MCP_XOXD_TOKEN = '';
+        }
+        if (process.env.SLACK_MCP_XOXP_TOKEN === '${user_config.xoxp_token}') {
+            process.env.SLACK_MCP_XOXP_TOKEN = '';
+        }
+        if (process.env.SLACK_MCP_ADD_MESSAGE_TOOL === '${user_config.add_message_tool}') {
+            process.env.SLACK_MCP_ADD_MESSAGE_TOOL = '';
+        }
+    }
+
     try {
         const binary = BINARY_MAP[`${process.platform}_${process.arch}`];
 
         if (process.env.SLACK_MCP_DXT) {
-            return require.resolve(process.cwd() + `/server/${binary.name}${binary.suffix}`);
+            return require.resolve(__dirname + `/${binary.name}${binary.suffix}`);
         } else {
             return require.resolve(`${binary.name}/bin/${binary.name}${binary.suffix}`);
         }
